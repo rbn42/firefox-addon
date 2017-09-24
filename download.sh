1daa12ccb1157d410930dba301da633e37da416b
rbn42 <bl100@students.waikato.ac.nz>
2017-09-25 03:19:53 +1300

    download.sh

---
+++ download.sh
@@ -0,0 +1,16 @@
+#Adblock Plus
+wget https://addons.mozilla.org/firefox/downloads/latest/adblock-plus/addon-1865-latest.xpi
+#Add to Search Bar
+https://addons.mozilla.org/firefox/downloads/latest/add-to-search-bar/addon-3682-latest.xpi
+#Classic Theme Restorer
+wget https://addons.mozilla.org/firefox/downloads/latest/classicthemerestorer/addon-472577-latest.xpi
+#Firebug
+wget https://addons.mozilla.org/firefox/downloads/latest/firebug/addon-1843-latest.xpi
+#Greasemonkey
+wget https://addons.mozilla.org/firefox/downloads/latest/greasemonkey/addon-748-latest.xpi
+#hide caption titlebar plus
+wget https://addons.mozilla.org/firefox/downloads/latest/hide-caption-titlebar-plus-sma/addon-13505-latest.xpi
+#Self-Destructing Cookies
+wget https://addons.mozilla.org/firefox/downloads/latest/self-destructing-cookies/addon-415846-latest.xpi
+#VimFx
+wget https://addons.mozilla.org/firefox/downloads/latest/vimfx/addon-404785-latest.xpi
