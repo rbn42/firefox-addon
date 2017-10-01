faf4279a05858eb6b27ffc1715fd44609bc35992
rbn42 <bl100@students.waikato.ac.nz>
2017-10-02 08:55:54 +1300

    download.sh

--- xpi/download.sh
+++ xpi/download.sh
@@ -21,6 +21,8 @@ wget https://addons.mozilla.org/firefox/downloads/latest/cookies-manager-plus/ad
 
 wget https://addons.mozilla.org/firefox/downloads/latest/cookieculler/addon-82-latest.xpi
 
+wget https://addons.mozilla.org/firefox/downloads/latest/cookiekeeper/platform:2/addon-481410-latest.xpi
+
 #arc theme
 wget https://addons.mozilla.org/firefox/downloads/file/607619/arc_theme-52.20170311-fx-linux.xpi
 wget https://addons.mozilla.org/firefox/downloads/file/607620/arc_darker_theme-52.20170311-fx-linux.xpi
